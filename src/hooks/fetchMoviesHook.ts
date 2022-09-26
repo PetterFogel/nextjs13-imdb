cf337452f593237d97d76c92e068161356d3937e
PetterFogel <petterfogel96@gmail.com>
2022-09-26 19:31:14 +0200

    Implement .env file with api key variable

--- src/hooks/fetchMoviesHook.ts
+++ src/hooks/fetchMoviesHook.ts
@@ -1,15 +1,13 @@
 import { MoviesResponse } from "../../models/movie";
 import useSWR from "swr";
 
-const API_KEY: string = "46eda78843a870bbbc6d78ac7c1be16d";
-
 const category = "popular";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export const useFetchMovies = (): MoviesResponse => {
   const { data, error } = useSWR(
-    `https://api.themoviedb.org/3/movie/${category}?api_key=${API_KEY}&language=en-US&page=1`,
+    `https://api.themoviedb.org/3/movie/${category}?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&page=1`,
     fetcher
   );
 
