1beedc861dc82883527901216c32bfdfc6443e73
PetterFogel <petterfogel96@gmail.com>
2022-10-02 19:13:53 +0200

    Implement generic movei slider + default settings

---
+++ src/common/constants/sliderSettings.ts
@@ -0,0 +1,34 @@
+import { Settings } from "react-slick";
+
+export const sliderSettings: Settings = {
+  infinite: false,
+  speed: 500,
+  slidesToShow: 5,
+  slidesToScroll: 5,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+      },
+    },
+  ],
+};
--- src/pages/home-page/HomeList.tsx
+++ src/pages/home-page/HomeList.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { Movie } from "../../../models/movie";
 import { MovieItem } from "../../common/components/movie-item/MovieItem";
+import { MoviesSlider } from "../../common/components/movies-slider/MoviesSlider";
 import { PageTitle } from "../../common/components/page-title/PageTitle";
 import styles from "./style/Home.module.css";
 
@@ -12,11 +13,7 @@ export const HomeList: FC<HomeListProps> = ({ movies }) => {
   return (
     <>
       <PageTitle title={"Upcoming:"} />
-      <div className={styles.movieListContainer}>
-        {movies.map((movie) => (
-          <MovieItem key={movie.id} movie={movie} />
-        ))}
-      </div>
+      <MoviesSlider movies={movies} />
     </>
   );
 };
