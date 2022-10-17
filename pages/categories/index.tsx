3b895babada812b8645e126ff93ba1c48f65d12c
PetterFogel <petterfogel96@gmail.com>
2022-10-17 19:52:24 +0200

    Implement logic to fetch top rated movies

--- pages/categories/index.tsx
+++ pages/categories/index.tsx
@@ -1,10 +1,21 @@
 import { NextPage } from "next";
+import { Loader } from "../../src/common/components/Loader/Loader";
+import { MoviesSlider } from "../../src/common/components/movies-slider/MoviesSlider";
+import { PageTitle } from "../../src/common/components/page-title/PageTitle";
+import { CategoryTypes } from "../../src/common/constants/enums";
+import { useFetchMovies } from "../../src/hooks/fetchMoviesHook";
+import styles from "./style/CategoriesStyle.module.css";
 
 const CategoriesPage: NextPage = () => {
+  const { data, isLoading, error } = useFetchMovies(CategoryTypes.TopRated);
+
+  if (error) return <h2>Something went wrong...</h2>;
+
   return (
-    <>
-      <h1 style={{ color: "#fff", fontWeight: 500 }}>Categories</h1>
-    </>
+    <div className={styles.root}>
+      <PageTitle title="Top rated" />
+      {isLoading ? <Loader /> : <MoviesSlider movies={data.results} />}
+    </div>
   );
 };
 
