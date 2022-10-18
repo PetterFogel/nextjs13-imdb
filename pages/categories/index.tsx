005dfe38207bee1ae3750153d20ac993f9843cd9
PetterFogel <petterfogel96@gmail.com>
2022-10-18 18:06:11 +0200

    Removed unused imports and console logs

--- pages/categories/index.tsx
+++ pages/categories/index.tsx
@@ -1,10 +1,8 @@
 import { NextPage } from "next";
 import { Movie } from "../../models/movie";
-import { Loader } from "../../src/common/components/Loader/Loader";
 import { MoviesSlider } from "../../src/common/components/movies-slider/MoviesSlider";
 import { PageTitle } from "../../src/common/components/page-title/PageTitle";
 import { CategoryTypes } from "../../src/common/constants/enums";
-import { useFetchMovies } from "../../src/hooks/fetchMoviesHook";
 import styles from "./style/CategoriesStyle.module.css";
 
 interface CategoriesPageProps {
@@ -26,7 +24,6 @@ export const getStaticProps = async () => {
     `https://api.themoviedb.org/3/movie/${CategoryTypes.TopRated}?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&page=1`
   );
   const data = await res.json();
-  console.log(data.results);
 
   return {
     props: {
