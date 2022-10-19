417f7ab4e24420b76113281d07fb13fb1dc6365b
PetterFogel <petterfogel96@gmail.com>
2022-10-19 19:01:11 +0200

    Test vercel

--- pages/categories/index.tsx
+++ pages/categories/index.tsx
@@ -9,27 +9,27 @@ interface CategoriesPageProps {
   movies: Movie[] | undefined;
 }
 
-const CategoriesPage: NextPage<CategoriesPageProps> = ({ movies }) => {
-  if (!movies) return <p>Something went wrong...</p>;
+const CategoriesPage: NextPage<CategoriesPageProps> = () => {
+  // if (!movies) return <p>Something went wrong...</p>;
 
   return (
     <div className={styles.root}>
       <PageTitle title="Top rated" />
-      <MoviesSlider movies={movies} />
+      {/* <MoviesSlider movies={movies} /> */}
     </div>
   );
 };
 
-export const getStaticProps = async () => {
-  const res = await fetch(
-    `https://api.themoviedb.org/3/movie/${CategoryTypes.TopRated}?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&page=1`
-  );
-  const data = await res.json();
-  return {
-    props: {
-      movies: data.results ?? null,
-    },
-  };
-};
+// export const getStaticProps = async () => {
+//   const res = await fetch(
+//     `https://api.themoviedb.org/3/movie/${CategoryTypes.TopRated}?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&page=1`
+//   );
+//   const data = await res.json();
+//   return {
+//     props: {
+//       movies: data.results ?? null,
+//     },
+//   };
+// };
 
 export default CategoriesPage;
