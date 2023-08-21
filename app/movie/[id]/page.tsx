7633af6d3b44ccf8358ebf412146427d0a834d6d
PetterFogel <petterfogel96@gmail.com>
2023-08-21 15:02:23 +0200

    Implement logic to fetch specific movie

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -1,9 +1,26 @@
+import { getMovie } from "@/utils/fetchData";
+
 interface Props {
   params: { id: string };
 }
 
-const Movie = ({ params: { id } }: Props) => {
-  return <div>{id}</div>;
+const Movie = async ({ params: { id } }: Props) => {
+  const movie = await getMovie(id);
+
+  return (
+    <section>
+      <h2>{movie.title}</h2>
+      <div>{movie.overview}</div>
+      <div>{movie.poster_path}</div>
+      <div>{movie.vote_average}</div>
+      <ul>
+        {movie.genres.map((genre) => (
+          <li key={genre.id}>{genre.name}</li>
+        ))}
+      </ul>
+      <div>{movie.release_date}</div>
+    </section>
+  );
 };
 
 export default Movie;
--- utils/fetchData.ts
+++ utils/fetchData.ts
@@ -1,4 +1,8 @@
-export const fetchData = async (genre: string | undefined) => {
+import { IMovie, IMovies } from "@/types/movie";
+
+export const getMovies = async (
+  genre: string | undefined
+): Promise<IMovies> => {
   const res = await fetch(
     `https://api.themoviedb.org/3/movie/${genre}?api_key=${process.env.API_KEY}&language=en-US&page=1`
   );
@@ -7,4 +11,11 @@ export const fetchData = async (genre: string | undefined) => {
     throw new Error("Failed to fetch data");
   }
   return res.json();
+};
+
+export const getMovie = async (id: string): Promise<IMovie> => {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}`
+  );
+  return await res.json();
 };
