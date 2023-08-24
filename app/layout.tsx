51f640a7a44c678d15b34e9feee409064e22d5cb
PetterFogel <petterfogel96@gmail.com>
2023-08-24 10:29:38 +0200

    Implement max width styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,21 @@
-import Header from "@/components/header/Header";
-import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Header from "@/components/header/Header";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Flickify",
-  description: "This website is a competitor to imdb",
+  description: "This website is a competitor to imdb"
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        <main>{children}</main>
+        <main className="m-auto max-w-7xl p-4 lg:px-6">{children}</main>
       </body>
     </html>
   );
