c2b293a38a999351d4d26728b0ca6f66d1b4edc3
PetterFogel <petterfogel96@gmail.com>
2023-08-28 10:57:44 +0200

    Fixed typo for type

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,4 +1,4 @@
-import { FilterItem } from "@/types/filterItems";
+import { IFilterItem } from "@/types/filterItems";
 import { QueryParams } from "@/types/queryParams";
 
 export const queryParams: QueryParams = {
@@ -12,7 +12,7 @@ export const IMAGE_URL = "https://image.tmdb.org/t/p/original";
 export const EMPTY_MOVIE_URL =
   "https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg";
 
-export const filterItems: FilterItem[] = [
+export const filterItems: IFilterItem[] = [
   {
     title: "Popular",
     path: "/search/popular"
