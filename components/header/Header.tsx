7cd4b5d953af008944a73d529a26d9ee2aa3fbe9
PetterFogel <petterfogel96@gmail.com>
2024-01-20 13:04:48 +0100

    Minor style refactoring

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -30,7 +30,7 @@ const Header = () => {
         <Link
           href="/watchlist"
           className="group flex gap-1 text-neutral-400 underline-offset-4 hover:text-white hover:underline">
-          Wathlist{" "}
+          <span className="hidden md:block">Wathlist </span>
           <BookmarkIcon className="h-6 cursor-pointer text-neutral-400 group-hover:text-white" />
         </Link>
       </div>
